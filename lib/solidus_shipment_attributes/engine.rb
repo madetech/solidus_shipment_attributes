b6de33485d5ca1c92060ff003fbd1eec42048b97
Seb Ashton <seb@sebashton.com>
2016-09-28 15:02:25 +0100

    Add description to admin

--- lib/solidus_shipment_attributes/engine.rb
+++ lib/solidus_shipment_attributes/engine.rb
@@ -12,6 +12,10 @@ module SolidusShipmentAttributes
       if Spree::ShippingMethod.methods.include?(:with_translations)
         Spree::ShippingMethod.include(SolidusShipmentAttributes::ShipmentMethodAttributes)
       end
+
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/overrides/*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
     end
 
     config.to_prepare(&method(:activate).to_proc)
