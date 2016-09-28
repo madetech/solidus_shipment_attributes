24a92ee076d3788a69368fd9dc9fc42b41867472
Seb Ashton <seb@sebashton.com>
2016-09-28 15:48:48 +0100

    Rename spec

--- spec/order/show_spec.rb
+++ spec/order/show_spec.rb
@@ -8,14 +8,29 @@ describe 'Order shipment description', type: :request do
   before do
     shipment.order.user = user
     user.generate_spree_api_key!
+
+    get("/api/orders/#{shipment.order.number}", token: user.spree_api_key)
   end
 
   it 'displays the shipping method description' do
-    get("/api/orders/#{shipment.order.number}", token: user.spree_api_key)
-
-    shipping_methods = JSON.parse(response.body)['shipments'][0]['shipping_methods']
+    shipping_methods = order_shipment['shipping_methods']
 
     expect(shipping_methods.map(&:keys).flatten.uniq).to include('description')
     expect(shipping_methods.map{ |s| s['description'] }.flatten.uniq).to include('Super Made Tech shipping method')
+  end
+
+  it 'displays the shipping rate description' do
+    shipping_rates = order_shipment['shipping_rates']
+
+    expect(shipping_rates.map(&:keys).flatten.uniq).to include('description')
+    expect(shipping_rates.map{ |s| s['description'] }.flatten.uniq).to include('Super Made Tech shipping method')
+  end
+
+  def order_shipment
+    parsed_response['shipments'].first
+  end
+
+  def parsed_response
+    JSON.parse(response.body)
   end
 end
